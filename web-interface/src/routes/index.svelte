a8be017404c5a0aa167372a0aab26a1fbc4f1399
Tim N <tim@gurka.se>
2022-03-24 17:13:17 +0100

    show throne power pts in web interface

--- web-interface/src/routes/index.svelte
+++ web-interface/src/routes/index.svelte
@@ -1,15 +1,15 @@
 <script>
 
 	let promise;
-	let thrones_text;
 
 	async function genNewThrones() {
 		const response = await fetch('/gen.pl');
-		const text = await response.text();
+		const json = await response.json();
 		if ( response.ok ) {
-			thrones_text = text;
+			console.log(json.thrones);
+			return json.thrones;
 		} else {
-			throw new Error(text);
+			throw new Error(json);
 		}
 	}
 
@@ -24,6 +24,23 @@
 
 <button on:click={clickButton}>generate thrones</button>
 
-{#if thrones_text}
-	<p style="white-space:pre;">{thrones_text}</p>
+{#if promise}
+	{#await promise}
+		<p>generating...</p>
+	{:then thrones}
+		{#each thrones as throne}
+			<div>
+				<p>Throne</p>
+				<ul>
+				{#each throne.powers as power}
+					<li>{power.title} ({power.pts} pts)</li>
+				{/each}
+				</ul>
+			</div>
+		{/each}
+	{:catch error}
+		<p style="color:red;">
+			<span style="font-weight:bold;">Error: {error.message}</span>
+		</p>
+	{/await}
 {/if}
--- web-interface/static/gen.pl
+++ web-interface/static/gen.pl
@@ -4,6 +4,7 @@ use 5.30.0;
 use warnings;
 
 use CGI::Simple;
+use JSON qw/encode_json/;
 
 use lib '/srv/http/thronegen.illwiki.com/public_html';
 #use lib '../..';
@@ -13,16 +14,27 @@ use ThroneGen::DM;
 # write headers
 
 my $cgi = CGI::Simple->new;
-print $cgi->header('text/plain');
+print $cgi->header('application/json');
 
 # generate five 5pt thrones
 
 my @thrones = map { ThroneGen::Throne->new( pts => 5 ) } ( 1..5 );
 
-# print thrones
+# make JSON
+
+my @j_thrones;
 
 for my $throne ( @thrones ) {
-	say "Throne";
-	say "  " . $_->title for ( @{$throne->powers} );
-	say "";
+	my $j_throne = {
+		pts => $throne->pts,
+		powers => [
+			map { {
+				pts => $_->pts,
+				title => $_->title,
+			} } @{$throne->powers}
+		]
+	};
+	push @j_thrones, $j_throne;
 }
+
+print encode_json( { thrones => \@j_thrones } );
