235163e652832b181c6497ad198348d1bc30c744
Tim N <tim@gurka.se>
2022-03-26 10:58:24 +0100

    display dm on side

--- web-interface/src/routes/index.svelte
+++ web-interface/src/routes/index.svelte
@@ -24,10 +24,12 @@
 
 <button on:click={clickButton}>generate thrones</button>
 
+<div id="container">
 {#if promise}
 	{#await promise}
 		<p>generating...</p>
 	{:then generated}
+		<div id="thrones">
 		{#each generated.thrones as throne}
 			<div>
 				<p>Throne</p>
@@ -38,18 +40,32 @@
 				</ul>
 			</div>
 		{/each}
-		<textarea>{generated.dm}</textarea>
+		</div>
+		<div id="dm">
+			<textarea>{generated.dm}</textarea>
+		</div>
 	{:catch error}
 		<p style="color:red;">
 			<span style="font-weight:bold;">Error: {error.message}</span>
 		</p>
 	{/await}
 {/if}
+</div>
 
 
 <style>
+	#thrones {
+		float:left;
+		padding:16px;
+	}
+	#dm {
+		overflow:hidden;
+		max-width:480px;
+		height:480px;
+		margin-left:64px;
+	}
 	textarea {
 		width:100%;
-		height:640px;
+		height:100%;
 	}
 </style>
