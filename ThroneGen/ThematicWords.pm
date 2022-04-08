1177f68b7a531849c6543e98ca61e5778dda7fd4
Tim N <tim@gurka.se>
2022-04-08 14:12:48 +0200

    moved theme words to yaml file

--- ThroneGen/ThematicWords.pm
+++ ThroneGen/ThematicWords.pm
@@ -3,21 +3,15 @@ package ThroneGen::ThematicWords;
 use MooseX::Singleton;
 use namespace::autoclean;
 use Carp;
+use YAML qw//;
+use Data::Dumper qw(Dumper);
 
 has 'words' => (
 	is => 'ro',
 	isa => 'HashRef',
-	default => sub { {
-		# TODO move words to data file
-		fire => ['Fire','Flames','Brass','Summer','the Sun','Deserts','Dragons','the Phoenix','Magma','Scorching Winds', 'Scorching Storms','Suns','Rage','Gold','Rubies','Sunlight','Volcanoes','Heat'],
-		air => ['Air','the Sky'],
-		water => ['Water','the Ocean'],
-		earth => ['Earth','Earthquakes'],
-		astral => ['Pearls','Time'],
-		death => ['Death','Shadows'],
-		nature => ['Nature','Forests'],
-		blood => ['Blood'],
-	} },
+	default => sub {
+		return YAML::LoadFile( 'ThroneGen/data/thematic_words.yaml' );
+	},
 );
 
 sub word_on_theme {
