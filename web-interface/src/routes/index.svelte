bb62bad1a7663b72d4e631c48eb08826b7b45df0
Tim N <tim@gurka.se>
2022-04-24 23:22:03 +0200

    generate new thrones when num_thrones dropdown changes

--- web-interface/src/routes/index.svelte
+++ web-interface/src/routes/index.svelte
@@ -31,7 +31,7 @@
 <h1>Throne Generator</h1>
 
 <div id="buttondiv">
-	<select bind:value={num_thrones}>
+	<select bind:value={num_thrones} on:change={() => promise = genNewThrones()}>
 		{#each [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] as n}
 			<option value={n}>{n} throne{#if n!=1}s{/if}</option>
 		{/each}
