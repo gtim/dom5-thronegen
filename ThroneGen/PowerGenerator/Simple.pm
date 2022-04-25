b0d1e6402526402bdeb751ec5fbf4c6e62e9ec72
Tim N <tim@gurka.se>
2022-04-25 18:08:57 +0200

    fix TG::PG::Simple weight argument bug

--- ThroneGen/PowerGenerator/Simple.pm
+++ ThroneGen/PowerGenerator/Simple.pm
@@ -33,9 +33,17 @@ around BUILDARGS => sub {
 	for my $required_arg ( qw/pts type title themes/ ) {
 		croak "$required_arg not defined in TG::PG::Simple constructor (title: $args{title})" unless defined $args{$required_arg};
 	}
+
+	my %weight_arg = ();
+	if ( exists $args{weight} ) {
+		%weight_arg = ( weight => $args{weight} );
+		delete $args{weight}
+	}
+
 	return $class->$orig(
 		pts_allowed => [ $args{pts} ],
 		possible_themes => $args{themes},
+		%weight_arg,
 		_power_arguments => \%args,
 	);
 };
